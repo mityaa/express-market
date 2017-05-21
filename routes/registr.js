4b7b1ac09aa3e59836897a62136bb6ef9be5c8da
mitya <m.nenilin@avia-centr.ru>
2017-05-21 15:58:53 +0300

    new registr route and form login/registr

--- routes/registr.js
+++ routes/registr.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var sql = require('./../classes/getContent');
-var con = new sql('root', '123456');
 
 router.get('/', function (req, res) {
     res.render('loginForm', { formText: 'Форма входа' });
